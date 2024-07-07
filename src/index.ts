39da74a7a1bfc83ef18afc3df00ea9ee8a95af7d
aquapi <aquaplmc@gmail.com>
2024-07-07 22:24:31 +0700

    Inform success

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import { Byte, cors } from '@bit-js/byte';
+import { Byte, cors, send } from '@bit-js/byte';
 import api from './api';
 
 const app = new Byte()
   .use(cors())
+  .get('/', send.body('This is the backend of Dailyecons'))
   .route('/api', api);
 
 export default {
