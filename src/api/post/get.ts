92f869924279be075665d0be45fdf8c3c0af23e0
aquapi <aquaplmc@gmail.com>
2024-07-28 09:59:44 +0700

    Add /fromAdmin

--- src/api/post/get.ts
+++ src/api/post/get.ts
@@ -1,12 +1,45 @@
 import { Byte, query } from '@bit-js/byte';
 import db from '@db';
+import { parseUnsign } from '@utils/token';
 
 const parseQuery = query.schema({
   limit: { type: 'number' },
   startID: { type: 'number' }
 });
 
+const getAdminPost = new Byte()
+  .state('admin', parseUnsign)
+  .get('/', async (ctx) => {
+    try {
+      const query = parseQuery(ctx);
+
+      if (query !== null) {
+        const { limit, startID } = query;
+
+        if (limit > 0 && limit < 30)
+          return ctx.json(startID === -1
+            ? (await db.execute({
+              sql: 'SELECT rowid as id, title, content, date, author, readTimeApproximation, bannerLink, avatarLink, views FROM posts INNER JOIN admins ON posts.author = admins.name WHERE posts.author = ? ORDER BY rowid DESC LIMIT ?',
+              args: [ctx.admin, limit]
+            })).rows
+            : (await db.execute({
+              sql: 'SELECT rowid as id, title, content, date, author, readTimeApproximation, bannerLink, avatarLink, views FROM posts INNER JOIN admins ON posts.author = admins.name WHERE posts.author = ? AND rowid < ? ORDER BY rowid DESC LIMIT ?',
+              args: [ctx.admin, startID, limit]
+            })).rows
+          );
+      }
+
+      ctx.status = 400;
+    } catch (e) {
+      ctx.status = 500;
+    }
+
+    return ctx.end();
+  });
+
 export default new Byte()
+  .route('/fromAdmin', getAdminPost)
+
   .get('/', async (ctx) => {
     try {
       const query = parseQuery(ctx);
